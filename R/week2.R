1e950ebb5ac66d3be27aff56ca9e1828e648e0b9
Richard Landers <rlanders@tntlab.org>
2023-01-30 12:32:28 -0600

    Final for realsies

--- R/week2.R
+++ R/week2.R
@@ -5,7 +5,7 @@ rt_df$gender <- factor(rt_df$gender, levels=c("M","F","N"), labels=c("Male","Fem
 
 # Analysis
 mean(rt_df$rt)
-rt_f_df <- rt_df[rt_df$gender == "Female"]
+rt_f_df <- rt_df[rt_df$gender == "Female",]
 hist(rt_f_df$rt)
 datasets <- list(rt_df, rt_f_df)
 datasets[[1]]$rt
